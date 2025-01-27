937ad3e63e7e2e1da8e6044e7f9e4e818cdd11e1
tsfttb <vaczula.zsombor07@gmail.com>
2025-01-27 20:37:10 +0100

    added Titanium bar and ore

--- src/main/java/net/tsfttb/terrarium/Terrarium.java
+++ src/main/java/net/tsfttb/terrarium/Terrarium.java
@@ -2,16 +2,15 @@ package net.tsfttb.terrarium;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tsfttb.terrarium.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 //comment
 public class Terrarium implements ModInitializer {
 	public static final String MOD_ID = "terrarium";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-
-	}
+	public void onInitialize() { ModItems.registeredModItems(); }
 }
---
+++ src/main/java/net/tsfttb/terrarium/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.tsfttb.terrarium.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.tsfttb.terrarium.Terrarium;
+
+public class ModItems {
+    public static final Item TITANIUM_ORE = registerItem("titanium_ore", new Item(new Item.Settings()));
+    public static final Item TITANIUM_BAR = registerItem("titanium_bar", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item)  {
+        return Registry.register(Registries.ITEM, Identifier.of(Terrarium.MOD_ID, name), item);
+    }
+
+    public static void registeredModItems() {
+        Terrarium.LOGGER.info("Registering Mod Items for" + Terrarium.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(TITANIUM_ORE);
+            entries.add(TITANIUM_BAR);
+        });
+    }
+}
